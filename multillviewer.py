894f6e3d5038c1c761bc4a3b89a7efaecd0913b7
Rowan-Bevilacqua-unitn <rowantree7777777@gmail.com>
2024-11-05 13:46:06 +0100

    removed comment

--- multillviewer.py
+++ multillviewer.py
@@ -259,8 +259,6 @@ while True:
         # newContentSize = (contentSize[0], contentSize[1] + 200)
         # window[column_key].set_size(newContentSize)
 
-        # commen
-
         continue
     else:
         word = values["wordinput"].strip()
